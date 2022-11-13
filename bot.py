358001328dec417ece9d5524b70b6c7a03473fa0
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-13 13:36:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import pyrogram
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, CallbackQuery
+import replicate
 
 app = Client(
     "AI Bot",
@@ -16,13 +17,24 @@ app = Client(
     api_hash = os.environ["API_HASH"],
 )
 
+os.environ['REPLICATE_API_TOKEN'] = ('98c5f3316a3844513979085dbd9621904dd71dbd')
+
+model = replicate.models.get("stability-ai/stable-diffusion")
+version = model.versions.get("8abccf52e7cba9f6e82317253f4a3549082e966db5584e92c808ece132037776")
+
+def stableimage(message, prompt):
+
+	output = version.predict(prompt=prompt)
+        await message.reply_photo(output)
+            
+
 # request data
 reqUrl = "https://backend.craiyon.com/generate"
 headersList = {"authority": "backend.craiyon.com", "accept": "application/json", "accept-language": "en-US,en;q=0.9", "cache-control": "no-cache", "content-type": "application/json", "dnt": "1", "origin": "https://www.craiyon.com", "pragma": "no-cache", "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "Linux", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-site", "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 #pretext = "data:image/jpeg;base64,"
 
 # getting images and uploding
-def genrateimages(message,prompt):
+def genrateimages(message,prompt,hooo):
 
 	# getting the response
 	payload = json.dumps({"prompt": prompt})
@@ -38,6 +50,7 @@ def genrateimages(message,prompt):
 		i = i + 1 
 	
 	# sending images
+        await hooo.message.delete()
 	app.send_media_group(message.chat.id,
     [
         InputMediaPhoto(f"{message.id}/1.jpeg", caption=prompt),
@@ -98,11 +111,13 @@ async def callback(bot, msg: CallbackQuery):
 # dalle command
 @app.on_message(filters.text)
 async def getpompt(client, message):
-     await message.reply("**Pʀᴏᴄᴇssɪɴɢ...**")
+     hooo = await message.reply("**Pʀᴏᴄᴇssɪɴɢ...**")
      prompt = message.text
      id = int(os.environ["ID"])
      await app.send_message(chat_id=id, text=f"**Uꜱᴇʀ ɴᴀᴍᴇ** : **{message.from_user.mention}**\n\n**Pʀᴏᴍᴘᴛ :** {message.text}")
-     ai = threading.Thread(target=lambda:genrateimages(message,prompt),daemon=True)
+     ai = threading.Thread(target=lambda:genrateimages(message,prompt,hooo),daemon=True)
+     si = threading.Thread(target=lambda:stableimage(message,prompt),daemon=True)
+     si.start()
      ai.start()
      
 #apprun
