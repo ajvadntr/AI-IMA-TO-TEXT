09180b5b45fdbdfdd861df33d730b36710461136
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-13 13:47:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ version = model.versions.get("8abccf52e7cba9f6e82317253f4a3549082e966db5584e92c8
 def stableimage(message, prompt):
 	output = version.predict(prompt=prompt)
         await message.reply_photo(output)
-
+##eeee
 reqUrl = "https://backend.craiyon.com/generate"
 headersList = {"authority": "backend.craiyon.com", "accept": "application/json", "accept-language": "en-US,en;q=0.9", "cache-control": "no-cache", "content-type": "application/json", "dnt": "1", "origin": "https://www.craiyon.com", "pragma": "no-cache", "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "Linux", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-site", "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 #pretext = "data:image/jpeg;base64,"
