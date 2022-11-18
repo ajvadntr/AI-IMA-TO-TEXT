354351dd07a0fb1e80afd9c6ce9cfcfbdb539347
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-18 22:59:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,6 +28,12 @@ koyeb = web.AppRunner(await web_server())
         bind_address = "0.0.0.0"
         await web.TCPSite(koyeb, bind_address, PORT).start()
 
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("aiom")
+
 os.environ['REPLICATE_API_TOKEN'] = ('98c5f3316a3844513979085dbd9621904dd71dbd')
 model = replicate.models.get("stability-ai/stable-diffusion")
 version = model.versions.get("8abccf52e7cba9f6e82317253f4a3549082e966db5584e92c808ece132037776")
