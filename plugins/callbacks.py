750faf676cd0ae9e7f5bdcd8a0cd06c6edae9eed
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-22 21:11:14 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -1,1 +1,15 @@
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+@app.on_callback_query()
+async def callback(bot, msg: CallbackQuery):
+    if msg.data == "toim":
+            await msg.message.edit("Sᴇɴᴅ Tᴇxᴛ Tᴏ Gᴇɴᴇʀᴀᴛᴇ Iᴍᴀɢᴇ")
+
+    if msg.data == "help":
+            await msg.message.edit("help")
+
+    if msg.data == "about":
+            await msg.message.edit("about")
+
+    if msg.data == "close":
+            await msg.message.delete()
