c9cb118d0abfbca88fd92a03af7e45cdd22e277d
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-22 21:09:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,19 +100,7 @@ async def start(client: pyrogram.client.Client, message: pyrogram.types.messages
         reply_markup=InlineKeyboardMarkup(button)
     )
 
-@app.on_callback_query()
-async def callback(bot, msg: CallbackQuery):
-    if msg.data == "toim":
-            await msg.message.edit("Sᴇɴᴅ Tᴇxᴛ Tᴏ Gᴇɴᴇʀᴀᴛᴇ Iᴍᴀɢᴇ")
 
-    if msg.data == "help":
-            await msg.message.edit("help")
-
-    if msg.data == "about":
-            await msg.message.edit("about")
-
-    if msg.data == "close":
-            await msg.message.delete()
 
 # dalle command
 @app.on_message(filters.text)
