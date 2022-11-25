559a36d4e0904e62cf54fe4b0fd97979160f6abc
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-25 20:39:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
+from aiohttp import web
+from plugins import web_server
+
 import os
-import io
-import json
-import base64
-import threading
-import shutil
-import requests
+import sys
 import pyrogram
+from datetime import datetime
+import pyromod.listen
 from pyrogram import Client
 from pyrogram import filters
-from pyrogram.types import InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, CallbackQuery
-import replicate
 from config import PORT
+from pyrogram.enums import ParseMode
 
 class app(Client):
     def __init__(self):
@@ -24,7 +23,23 @@ class app(Client):
             workers= "4",
             bot_token= "5676297902:AAGYG022_mQe0VhLdzdEGsQZATGVc9NCa6Q"
         )
+        self.LOGGER = LOGGER
 
-    
-#apprun
-app.run()
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+
+        self.uptime = datetime.now()
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"aiombots")
+        self.username = usr_bot_me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
