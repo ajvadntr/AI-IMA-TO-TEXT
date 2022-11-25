06439375fed3207c9ae903aeb857ea4ccc314129
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-25 20:25:56 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,20 @@
+## --- ## ## --- ##
+## --- ## ## --- ##
+
+from bot import app
+import threading
+import pyrogram
+from pyrogram import filters
+from plugins.dall-mini import genrateimages
+
+## --- ## ## --- ##
+## --- ## ## --- ##
+
+@app.on_message(filters.text)
+async def getpompt(client, message):
+     replytext = await message.reply("**Pʀᴏᴄᴇssɪɴɢ...**")
+     prompt = message.text
+     id = "-1001683525472"
+     await app.send_message(chat_id=id, text=f"**Uꜱᴇʀ ɴᴀᴍᴇ** : **{message.from_user.mention}**\n\n**Pʀᴏᴍᴘᴛ :** {message.text}")
+     ai = threading.Thread(target=lambda:genrateimages(message,prompt),daemon=True)
+     ai.start()
