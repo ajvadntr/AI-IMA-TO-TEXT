01e7690be6ff3a2eed40c404d88cb2d639e84b45
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-26 11:56:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,7 +5,7 @@ from bot import Bot as app
 import threading
 import pyrogram
 from pyrogram import filters
-from plugins.dall-mini import genrateimages
+from .dall-mini import genrateimages
 
 ## --- ## ## --- ##
 ## --- ## ## --- ##
