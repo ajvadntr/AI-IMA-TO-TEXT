cff840f8a889755bb785f97364bcf90332134a64
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-26 16:17:45 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -1,11 +1,21 @@
 #©aiom|bots
 
-
+import os
+import json
+import base64
+import threading
+import shutil
+import requests
+import pyrogram
 from bot import Bot as app
 from config import DBID
 import asyncio
 from asyncio import TimeoutError
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+reqUrl = "https://backend.craiyon.com/generate"
+headersList = {"authority": "backend.craiyon.com", "accept": "application/json", "accept-language": "en-US,en;q=0.9", "cache-control": "no-cache", "content-type": "application/json", "dnt": "1", "origin": "https://www.craiyon.com", "pragma": "no-cache", "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "Linux", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-site", "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+
 
 @app.on_callback_query()
 async def callback(bot, msg: CallbackQuery):
