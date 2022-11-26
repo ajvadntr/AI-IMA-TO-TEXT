2b831f32c901d1aa50e53bcbf013b6f6edcd5ba0
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-26 13:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ PORT = os.environ.get("PORT", "8080")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
+DBID = int(os.environ.get("DBID", "-1001683525472"))
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
