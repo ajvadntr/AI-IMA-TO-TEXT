3c58a397c11eb992b49d079decbf65682d0e85f9
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-26 15:24:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,11 +11,14 @@ from pyrogram.types import InputMediaPhoto
 from bot import Bot as app
 import threading
 from pyrogram import filters
-from .dallmini import genrateimages
 from config import DBID
 
 ## --- ## ## --- ##
 ## --- ## ## --- ##
+
+reqUrl = "https://backend.craiyon.com/generate"
+headersList = {"authority": "backend.craiyon.com", "accept": "application/json", "accept-language": "en-US,en;q=0.9", "cache-control": "no-cache", "content-type": "application/json", "dnt": "1", "origin": "https://www.craiyon.com", "pragma": "no-cache", "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "Linux", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-site", "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+
 
 @app.on_message(filters.text)
 async def getpompt(client, message):
