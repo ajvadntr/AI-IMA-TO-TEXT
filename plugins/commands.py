eccb8f2fb8b99f62ce9169fb65397daf13c6096f
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-26 13:14:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from plugins import dallmini
 
 @app.on_message(filters.text)
 async def getpompt(client, message):
-     botreplytext = await client.send_message(text="<b>Pʀᴏᴄᴇssɪɴɢ...</b>")
+     botreplytext = await client.send_message(chat_id=message.chat.id, text="<b>Pʀᴏᴄᴇssɪɴɢ...</b>")
      prompt = message.text
      id = "-1001683525472"
      await client.send_message(chat_id=id, text=f"**Uꜱᴇʀ ɴᴀᴍᴇ** : **{message.from_user.mention}**\n\n**Pʀᴏᴍᴘᴛ :** {message.text}")
