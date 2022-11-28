ed1c15b5986e999459a745b3b1b26663872594b6
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-28 22:18:43 +0530

    Update stablediffusion.py

--- plugins/stablediffusion.py
+++ plugins/stablediffusion.py
@@ -5,19 +5,19 @@ import os
 from PIL import Image
 import base64
 import replicate
+import json
+import base64
 
 os.environ['REPLICATE_API_TOKEN'] = ('98c5f3316a3844513979085dbd9621904dd71dbd')
 model = replicate.models.get("stability-ai/stable-diffusion")
 version = model.versions.get("8abccf52e7cba9f6e82317253f4a3549082e966db5584e92c808ece132037776")
 
 
-def image_to_bytes(output):
-    bio = BytesIO()
-    bio.name = 'image.jpeg'
-    Image.save(bio, 'JPEG')
-    bio.seek(0)
-    return bio
 
 def stablediffusion(bot, msg, prompt):
     output = version.predict(prompt=prompt)
-    bot.send_photo(msg.from_user.id, image=image_to_bytes(output))
+    data = {}
+    with open('{msg.id}.jpeg', mode='wb') as file:
+        img = file.read()
+    data['img'] = base64.encodebytes(img).decode('utf-8')
+    bot.send_photo(msg.from_user.id, image=json.dumps(data))
