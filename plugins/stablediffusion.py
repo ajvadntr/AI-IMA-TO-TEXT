da81fda731ba33fc5dff22c544cef70ae5dd3a59
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-11-28 22:32:21 +0530

    Update stablediffusion.py

--- plugins/stablediffusion.py
+++ plugins/stablediffusion.py
@@ -7,6 +7,7 @@ import base64
 import replicate
 import json
 import base64
+from pyrogram.types import InputMediaPhoto
 
 os.environ['REPLICATE_API_TOKEN'] = ('98c5f3316a3844513979085dbd9621904dd71dbd')
 model = replicate.models.get("stability-ai/stable-diffusion")
@@ -15,9 +16,22 @@ version = model.versions.get("8abccf52e7cba9f6e82317253f4a3549082e966db5584e92c8
 
 
 def stablediffusion(bot, msg, prompt):
-    output = version.predict(prompt=prompt)
+    output = version.predict(prompt=prompt, num_outputs=9)
+    i = 1
     for ele in output["images"]:
 	    image = base64.b64decode(ele.replace('\\n',''))
-	    with open(f"{msg.id}/1.jpeg","wb") as file:
+	    with open(f"{msg.id}/{i}.jpeg","wb") as file:
 	            file.write(image)
-    bot.send_photo(msg.from_user.id, image="{msg.id}/1.jpeg")
+            i = i + 1
+    bot.send_media_group(msg.from_user.id,
+    [
+        InputMediaPhoto(f"{msg.id}/1.jpeg", caption=prompt),
+        InputMediaPhoto(f"{msg.id}/2.jpeg", caption=prompt),
+        InputMediaPhoto(f"{msg.id}/3.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/4.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/5.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/6.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/7.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/8.jpeg", caption=prompt),
+		InputMediaPhoto(f"{msg.id}/9.jpeg", caption=prompt)
+    ]
